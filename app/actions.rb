5a73076aae71f58e0c96567073f604267157f7b6
DJINA <pajkic.djina@gmail.com>
2016-02-20 21:06:36 +0000

    added messages and message form

--- app/actions.rb
+++ app/actions.rb
@@ -4,6 +4,13 @@ get '/' do
 end
 
 #this adds a path on how to get to the messages page
-get '/messages' do 
-  'Message List here'
+get '/messages' do   #this will show up in http://localhost:3000/message
+  #'Message List here'  #this is a hardcoded string, we want it to render the correct HTML
+  @messages = Message.all  #sinatra will make this @messages variable
+  erb :'messages/index'   #available in messages/index.erb
 end
+
+get '/new' do
+  erb :'messages/new'
+end
+
